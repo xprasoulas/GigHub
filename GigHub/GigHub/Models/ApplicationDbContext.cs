753a1edbf5edf37862fd4da53a2b7ef7ac3bb569
Charis Prasoulas <xprasoulas@gmail.com>
2019-05-21 23:38:48 +0300

    Split and remove IdentityModels into two classes ApplicationDbContext  & ApplicationUser

---
+++ GigHub/GigHub/Models/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
+
+namespace GigHub.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public DbSet<Gig> Gigs { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+
+        public ApplicationDbContext()
+            : base("GigHubDb", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
--- GigHub/GigHub/Models/ApplicationUser.cs
+++ GigHub/GigHub/Models/ApplicationUser.cs
@@ -1,12 +1,10 @@
 ﻿using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
-using System.Data.Entity;
 using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace GigHub.Models
 {
-    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
@@ -15,22 +13,6 @@ namespace GigHub.Models
             var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
             // Add custom user claims here
             return userIdentity;
-        }
-    }
-
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
-    {
-        public DbSet<Gig> Gigs { get; set; }
-        public DbSet<Genre> Genres{ get; set; }
-
-        public ApplicationDbContext()
-            : base("GigHubDb", throwIfV1Schema: false)
-        {
-        }
-
-        public static ApplicationDbContext Create()
-        {
-            return new ApplicationDbContext();
         }
     }
 }
