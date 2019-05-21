f4056b28ad1a07b825e11f54f7f84a54ffd36a9e
Charis Prasoulas <xprasoulas@gmail.com>
2019-05-21 23:50:47 +0300

    Add Data Annotations for Override Convention for Gig And Genre

--- GigHub/GigHub/Models/Genre.cs
+++ GigHub/GigHub/Models/Genre.cs
@@ -1,8 +1,14 @@
-﻿namespace GigHub.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GigHub.Models
 {
     public class Genre
     {
+       
         public byte Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
 
     }
