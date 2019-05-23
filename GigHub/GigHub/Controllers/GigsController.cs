cd52e23cb16abf9d9bb7ea43e3516efc34256c4c
Charis Prasoulas <xprasoulas@gmail.com>
2019-05-23 22:07:19 +0300

    Change Action Name to Create and create a View for this action

--- GigHub/GigHub/Controllers/GigsController.cs
+++ GigHub/GigHub/Controllers/GigsController.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace GigHub.Controllers
 {
     public class GigsController : Controller
     {
         // GET: Gigs
-        public ActionResult Index()
+        public ActionResult Create()
         {
             return View();
         }
