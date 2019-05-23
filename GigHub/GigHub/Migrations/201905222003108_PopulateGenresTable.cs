24c5a834bace2acc131893a761dedfdfc68d1b00
Charis Prasoulas <xprasoulas@gmail.com>
2019-05-23 21:59:08 +0300

    Add SQL queries to add Genres in the table using the previous empty migration and update database

--- GigHub/GigHub/Migrations/201905222003108_PopulateGenresTable.cs
+++ GigHub/GigHub/Migrations/201905222003108_PopulateGenresTable.cs
@@ -7,10 +7,15 @@ namespace GigHub.Migrations
     {
         public override void Up()
         {
+            Sql("INSERT INTO Genres ( Id, Name) Values ( 1, 'Jazz')");
+            Sql("INSERT INTO Genres ( Id, Name) Values ( 2, 'Blues')");
+            Sql("INSERT INTO Genres ( Id, Name) Values ( 3, 'Rock')");
+            Sql("INSERT INTO Genres ( Id, Name) Values ( 4, 'Country')");
         }
         
         public override void Down()
         {
+            Sql("DELETE FROM Genres WHERE Id IN ( 1, 2, 3, 4)");
         }
     }
 }
