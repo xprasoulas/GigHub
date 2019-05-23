81cf97c5f3764c8b9484de53daa8162c6dfc2a9c
Charis Prasoulas <xprasoulas@gmail.com>
2019-05-23 22:04:49 +0300

    Add Controller Gigs

---
+++ GigHub/GigHub/Controllers/GigsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GigHub.Controllers
+{
+    public class GigsController : Controller
+    {
+        // GET: Gigs
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
