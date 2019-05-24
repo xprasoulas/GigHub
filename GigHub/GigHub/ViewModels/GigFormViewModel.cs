33a3dad769feb76e2c0a758b10514ada4df0e116
Charis Prasoulas <xprasoulas@gmail.com>
2019-05-24 22:47:07 +0300

    Create ViewModel Folder- Add class GigFormViewModel - change the view.. The use of it is to split DateTime to Date & Time

---
+++ GigHub/GigHub/ViewModels/GigFormViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GigHub.ViewModels
+{
+    public class GigFormViewModel
+    {
+        public string Venue { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+    }
+}
