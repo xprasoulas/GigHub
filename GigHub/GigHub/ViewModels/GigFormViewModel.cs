7102a311023e4b048f80a098d88c469959bd342e
Charis Prasoulas <xprasoulas@gmail.com>
2019-06-03 22:01:26 +0300

    Add Data Annotation to GigViewModel

--- GigHub/GigHub/ViewModels/GigFormViewModel.cs
+++ GigHub/GigHub/ViewModels/GigFormViewModel.cs
@@ -1,15 +1,24 @@
 ﻿using GigHub.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace GigHub.ViewModels
 {
     public class GigFormViewModel
     {
+        [Required]
         public string Venue { get; set; }
+
+        [Required]
         public string Date { get; set; }
+
+        [Required]
         public string Time { get; set; }
+
+        [Required]
         public byte Genre { get; set; }
+        
         public IEnumerable<Genre> Genres { get; set; }
 
         public DateTime DateTime
