250ca423a3e5bb1ecc102e12c939a1856f077081
Charis Prasoulas <xprasoulas@gmail.com>
2019-06-09 17:21:14 +0300

    Assign new property Name to user Add Migration Change View property userName to Name

--- GigHub/GigHub/Models/ApplicationUser.cs
+++ GigHub/GigHub/Models/ApplicationUser.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using System.ComponentModel.DataAnnotations;
 using System.Security.Claims;
 using System.Threading.Tasks;
 
@@ -7,6 +8,10 @@ namespace GigHub.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
