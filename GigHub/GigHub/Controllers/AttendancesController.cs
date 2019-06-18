823cbd7476a2deb0cfbe712c0c59083fea6d0879
Charis Prasoulas <xprasoulas@gmail.com>
2019-06-18 21:46:10 +0300

    Create an action for those who will attend at the Gig and then connect to a button

--- GigHub/GigHub/Controllers/AttendancesController.cs
+++ GigHub/GigHub/Controllers/AttendancesController.cs
@@ -1,13 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using GigHub.Models;
+using Microsoft.AspNet.Identity;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Results;
 
 namespace GigHub.Controllers
 {
+    [Authorize]
     public class AttendancesController : ApiController
     {
+        private ApplicationDbContext _context;
+
+        public AttendancesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Attend ([FromBody]int gigId)
+        {
+            var userId = User.Identity.GetUserId();
+            var exists = _context.Attendances.Any(a => a.AttendeeId == userId && a.GigId == gigId);
+
+            if (exists)
+                return BadRequest("The attendance already exist");
+
+            var attendance = new Attendance
+            {
+                GigId = gigId,
+                AttendeeId = userId
+            };
+
+            _context.Attendances.Add(attendance);
+            _context.SaveChanges();
+
+            return Ok();
+        }
     }
 }
